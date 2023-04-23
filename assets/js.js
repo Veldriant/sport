3237a494655fee9efb7b88b5b6312394dce4a0d2
Veldriant <65666734+Veldriant@users.noreply.github.com>
2023-04-23 20:57:31 +0300

    fix

---
+++ assets/js.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".mySwiper", {
+    loop:true,
+    slidesPerView: 4,
+    spaceBetween: 30,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+    });
+    
